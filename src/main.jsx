4b0e473bbc2e65f08730ea948d7c2def6d8cbf9b
larryruto98 <larryruto@gmail.com>
2024-12-17 14:12:22 +0300

    minor changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import '/home/larry/Development/code/HABIT-TRACKER/src/components/index.css';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
